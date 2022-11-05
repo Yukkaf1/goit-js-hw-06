8fb740b29b0c839125de3c8b2e794ba5270c6e74
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2022-11-05 12:41:24 +0100

    task-01-forEach

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,41 +3,38 @@
 
 
 const navEl = document.querySelectorAll("li.item");
-// console.log(navEl);
+console.log(navEl);
 console.log("Number of categories:", navEl.length);
 console.log("");
 
-// const El = document.querySelectorAll("li.item li");
-// console.log(El);
 
-// console.log(navEl[0]);
+// const navEl0Text = navEl[0].querySelector("h2");
+// console.log("Category:", navEl0Text.textContent);
 
-const navEl0Text = navEl[0].querySelector("h2");
-console.log("Category:", navEl0Text.textContent);
-
-const El0 = navEl[0].querySelectorAll("li");
-console.log("Elements:", El0.length);
-
-console.log("");
-
-const navEl1Text = navEl[1].querySelector("h2");
-console.log("Category:", navEl1Text.textContent);
-
-const El1 = navEl[1].querySelectorAll("li");
-console.log("Elements:", El1.length);
-
-console.log("");
-
-const navEl2Text = navEl[2].querySelector("h2");
-console.log("Category:", navEl2Text.textContent);
-
-const El2 = navEl[2].querySelectorAll("li");
-console.log("Elements:", El2.length);
+// const El0 = navEl[0].querySelectorAll("li");
+// console.log("Elements:", El0.length);
 
 // console.log("");
 
-// const navEl3Text = navEl[3].querySelector("h2");
-// console.log("Category:", navEl3Text.textContent);
+// const navEl1Text = navEl[1].querySelector("h2");
+// console.log("Category:", navEl1Text.textContent);
 
-// const El3 = navEl[3].querySelectorAll("li");
-// console.log("Elements:", El0.length);
+// const El1 = navEl[1].querySelectorAll("li");
+// console.log("Elements:", El1.length);
+
+// console.log("");
+
+// const navEl2Text = navEl[2].querySelector("h2");
+// console.log("Category:", navEl2Text.textContent);
+
+// const El2 = navEl[2].querySelectorAll("li");
+// console.log("Elements:", El2.length);
+
+navEl.forEach((el) => {
+    const textEl = el.querySelector("h2");
+    console.log("Category:", textEl.textContent);
+
+    const ulEl = el.querySelectorAll("li");
+    console.log("Elements:", ulEl.length);
+    console.log("");
+});
